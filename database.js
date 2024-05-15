b7b19cf1f05134f4ac5e0b7fec8f26233a98606b
tunyatornkia <91174718+tunyatornkia@users.noreply.github.com>
2024-05-15 16:51:20 +0700

    insert student form

---
+++ database.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+const mysql = require("mysql2/promise");
+const dbConn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root', // <== ระบุให้ถูกต้อง
+    password: '',  // <== ระบุให้ถูกต้อง
+    database: 'student_database',
+    port: 3306  // <== ใส่ port ให้ถูกต้อง (default 3306, MAMP ใช้ 8889)
+});
+app.post("/students", async (req, res) => {
+    // ส่งข้อมูลผ่าน body-parser (Middleware)
+    const name = req.body.name;
+    const age = req.body.age;
+    const phone = req.body.phone;
+    const email = req.body.email;
+ 
+    const connection = await dbConn
+    const rows = await connection.query("insert into students (name,age,phone,email) values('"+name+"','"+age+"',"+phone+",'"+email+"')")
+    // res.status(201).send(rows)
+    res.send("คุณได้ทำการเพิ่มข้อมูลเรียบร้อยแล้ว");
+ })
+app.get("/students", (req, res) => {
+    res.sendFile(__dirname + '/student.html');
+ });
+ 
+app.listen(3000, () => {
+    console.log("Server is running at port 3000")
+})
